0f747a5d10ebb18a8e06501062ef483baf71229e
Unknown <1643216@cgmatane.qc.ca>
2019-01-15 18:48:09 -0500

    Script physique ballottement de liquide

---
+++ Assets/Scripts/PhysiqueLiquide.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysiqueLiquide : MonoBehaviour {
+
+    public GameObject liquide;
+    public GameObject textureLiquide;
+
+    public int vitesseBalotementLiquide = 60;
+    public int vitesseRotationLiquide = 15;
+
+    public int difference = 25;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        baloterLiquide();
+
+        textureLiquide.transform.Rotate(Vector3.up * vitesseRotationLiquide * Time.deltaTime, Space.Self);
+    }
+
+    private void baloterLiquide()
+    {
+        Quaternion inverseRotationTasse = Quaternion.Inverse(transform.localRotation);
+
+        Vector3 rotationFinale = Quaternion.RotateTowards(liquide.transform.localRotation, inverseRotationTasse, vitesseBalotementLiquide * Time.deltaTime).eulerAngles;
+
+        rotationFinale.x = limiterRotation(rotationFinale.x, difference);
+        rotationFinale.y = limiterRotation(rotationFinale.y, difference);
+
+        liquide.transform.localEulerAngles = rotationFinale;
+    }
+
+    private float limiterRotation(float valeur, float difference)
+    {
+        if (valeur > 180)
+            return Mathf.Clamp(valeur, 360 - difference, 360);
+        else
+            return Mathf.Clamp(valeur, 0, difference);
+    }
+}
