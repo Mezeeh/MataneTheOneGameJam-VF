77885930a3936bf62284b5dbe08dbd93b0bf7367
Douvry <valentin.jabba@gmail.com>
2019-01-16 12:47:41 -0500

    deplacement chope

--- Assets/Scripts/DeplacementChope.cs
+++ Assets/Scripts/DeplacementChope.cs
@@ -6,16 +6,20 @@ public class DeplacementChope : MonoBehaviour {
     private float hAxis;
     public float vitesse;
     public float vitesseRotation;
-    private Vector3 input;
-    private bool isGrounded;
+    private Vector3 inputRotation;
+    private Vector3 inputForward;
+    private Vector3 inputHorizontal;
+    public bool isGrounded;
     private Rigidbody rb;
     public float hauteurSaut;
+    public float vitesseLaterale;
 
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();   
-	}
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        inputForward = new Vector3(0, 0, 1f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,10 +27,13 @@ public class DeplacementChope : MonoBehaviour {
         {
             var x = Input.GetAxis("HorizontalR");
             var y = Input.GetAxis("VerticalR");
-            input = new Vector3(x, 0, y);
-            transform.Rotate(input, vitesseRotation * Time.deltaTime);
-            input = new Vector3(0, 0, 1f);
-            transform.Translate(input * vitesse * Time.deltaTime, Space.World);
+            inputRotation = new Vector3(x, 0, y);
+            transform.Rotate(inputRotation, vitesseRotation * Time.deltaTime);
+            //transform.Translate(inputForward * vitesse * Time.deltaTime, Space.World);
+            //rb.velocity = inputForward * vitesse;
+            hAxis = Input.GetAxis("Horizontal");
+            inputHorizontal = new Vector3(hAxis * vitesseLaterale, 0, 0);
+            transform.Translate(inputHorizontal * Time.deltaTime, Space.World);
 
 
         }
@@ -34,12 +41,14 @@ public class DeplacementChope : MonoBehaviour {
         {
             if(Input.GetButtonDown("Jump"))
             {
+                Debug.Log("JUMP");
                 rb.AddForce(Vector3.up * hauteurSaut);
             }
             
             hAxis = Input.GetAxis("Horizontal");
-            input = new Vector3(hAxis, 0, 1f);
-            transform.Translate(input * vitesse * Time.deltaTime, Space.World);
+            inputHorizontal = new Vector3(hAxis * vitesseLaterale, 0, 0);
+            transform.Translate(inputHorizontal * Time.deltaTime, Space.World);
+            rb.velocity = inputForward * vitesse;
         }
        
 	}
