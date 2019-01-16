a7dd2b408014e2a499e1a9cddd91066a5796c689
Unknown <sedrickidd@hotmail.com>
2019-01-15 20:31:08 -0500

    Ajout du systeme de score

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+    public int scoreInitial;
+    public int pointARetirer;
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        score = scoreInitial;
+	}
+	
+	// Update is called once per frame
+    public void ReduireScore(int nombreAReduire)
+    {
+        score -= (nombreAReduire * pointARetirer);
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+    public void AjouterScore(int scoreAAjouter)
+    {
+        score += scoreAAjouter;
+    }
+
+}
