a8c2e293d26dcc194733b8b3a44c5d80f4c8f13e
Sedrick <sedrickidd@hotmail.com>
2019-01-16 02:32:11 -0500

    Ajout des classes de sauvegarde de scores + meta de fichier

---
+++ Assets/Scripts/ScoreSaveObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSaveObject
+{
+    public string nom;
+    public int score;
+
+}
---
+++ Assets/Scripts/XmlAccesseur.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class XmlAccesseur{
+
+	 private string scoreXml;
+     public static XmlAccesseur instance = null;
+    public XmlAccesseur()
+    {
+        scoreXml = @"../../Scores.xml";
+       
+    }
+    public static XmlAccesseur getInstance()
+    {
+        if(instance == null)
+            instance = new XmlAccesseur();
+        return instance;
+    }
+
+    public void save(List<ScoreSaveObject> scores)
+    {
+
+        try
+        {
+            if(File.Exists(scoreXml))
+            {
+                List<ScoreSaveObject> savedScores = load();
+                foreach(ScoreSaveObject score in scores)
+                {
+                    savedScores.Add(score);
+                }
+                XmlSerializer xSeriz = new XmlSerializer(typeof(List<ScoreSaveObject>));
+                FileStream fs = File.Create(scoreXml);
+                xSeriz.Serialize(fs, savedScores);
+                fs.Close();
+            }
+            else
+            {
+                XmlSerializer xSeriz = new XmlSerializer(typeof(List<ScoreSaveObject>));
+                FileStream fs = File.Create(scoreXml);
+                xSeriz.Serialize(fs, scores);
+                fs.Close();
+            }
+           
+        }
+        catch(Exception ex)
+        {
+            throw ex;
+        }
+    }
+    public List<ScoreSaveObject> load()
+    {
+        try
+        {
+            XmlSerializer reader = new XmlSerializer(typeof(List<ScoreSaveObject>));
+            StreamReader file = new StreamReader(scoreXml);
+            List<ScoreSaveObject> scores = (List<ScoreSaveObject>)reader.Deserialize(file);
+            file.Close();
+            return scores;
+        }
+        catch(Exception ex)
+        {
+            throw ex;
+        }
+        
+    }
+}
