715e3b8039217edc8de05851975a63d0a8045781
Unknown <1643216@cgmatane.qc.ca>
2019-01-15 19:29:36 -0500

    [Script] Adaptation niveau du liquide

--- Assets/Scripts/PhysiqueLiquide.cs
+++ Assets/Scripts/PhysiqueLiquide.cs
@@ -7,28 +7,31 @@ public class PhysiqueLiquide : MonoBehaviour {
     public GameObject liquide;
     public GameObject textureLiquide;
 
-    public int vitesseBalotementLiquide = 60;
+    public int vitesseBallottementLiquide = 60;
     public int vitesseRotationLiquide = 15;
 
-    public int difference = 25;
+    private int difference = 25;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float hauteurTasseRemplie = 1;
+    public float hauteurTasseVide = -1;
+
+    public float quantiteLiquide = 100;
+    public float quantiteMaxLiquide = 100;
 	
 	// Update is called once per frame
 	void Update () {
-        baloterLiquide();
+        ballotterLiquide();
+
+        quantifierTasse();
 
         textureLiquide.transform.Rotate(Vector3.up * vitesseRotationLiquide * Time.deltaTime, Space.Self);
     }
 
-    private void baloterLiquide()
+    private void ballotterLiquide()
     {
         Quaternion inverseRotationTasse = Quaternion.Inverse(transform.localRotation);
 
-        Vector3 rotationFinale = Quaternion.RotateTowards(liquide.transform.localRotation, inverseRotationTasse, vitesseBalotementLiquide * Time.deltaTime).eulerAngles;
+        Vector3 rotationFinale = Quaternion.RotateTowards(liquide.transform.localRotation, inverseRotationTasse, vitesseBallottementLiquide * Time.deltaTime).eulerAngles;
 
         rotationFinale.x = limiterRotation(rotationFinale.x, difference);
         rotationFinale.y = limiterRotation(rotationFinale.y, difference);
@@ -36,11 +39,32 @@ public class PhysiqueLiquide : MonoBehaviour {
         liquide.transform.localEulerAngles = rotationFinale;
     }
 
+    private void quantifierTasse()
+    {
+        float pourcentageLiquide = (quantiteLiquide / quantiteMaxLiquide);
+        //Debug.Log(pourcentageLiquide);
+        float positionLiquide = (pourcentageLiquide * (hauteurTasseRemplie - hauteurTasseVide)) + hauteurTasseVide;
+        Debug.Log(positionLiquide);
+
+        Vector3 hauteurLiquideFinal = liquide.transform.localPosition;
+        hauteurLiquideFinal.y = positionLiquide;
+
+        liquide.transform.localPosition = hauteurLiquideFinal; 
+    }
+
     private float limiterRotation(float valeur, float difference)
     {
         if (valeur > 180)
+        {
+
             return Mathf.Clamp(valeur, 360 - difference, 360);
+        }
         else
             return Mathf.Clamp(valeur, 0, difference);
+    }
+
+    private void renverserBiere()
+    {
+
     }
 }
