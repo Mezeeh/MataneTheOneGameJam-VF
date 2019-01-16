3c2d9d0655496c5036976290e74b82e0475b06d7
Unknown <1643216@cgmatane.qc.ca>
2019-01-15 23:34:30 -0500

    Menu principal

---
+++ Assets/Scripts/FenetrePrincipal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FenetrePrincipal : MonoBehaviour {
+
+	public void jouer()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitter()
+    {
+        Debug.Log("quitter()");
+        Application.Quit();
+    }
+}
