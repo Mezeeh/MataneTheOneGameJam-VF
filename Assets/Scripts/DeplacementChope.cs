4e1771c76971c9766b519c9b87827768af3e96eb
Unknown <sedrickidd@hotmail.com>
2019-01-15 20:06:53 -0500

    Ajout du saut dans le deplacement

--- Assets/Scripts/DeplacementChope.cs
+++ Assets/Scripts/DeplacementChope.cs
@@ -8,11 +8,13 @@ public class DeplacementChope : MonoBehaviour {
     public float vitesseRotation;
     private Vector3 input;
     private bool isGrounded;
+    private Rigidbody rb;
+    public float hauteurSaut;
 
 
 	// Use this for initialization
 	void Start () {
-         
+        rb = GetComponent<Rigidbody>();   
 	}
 	
 	// Update is called once per frame
@@ -22,21 +24,29 @@ public class DeplacementChope : MonoBehaviour {
             var x = Input.GetAxis("HorizontalR");
             var y = Input.GetAxis("VerticalR");
             input = new Vector3(x, 0, y);
-
             transform.Rotate(input, vitesseRotation * Time.deltaTime);
+            input = new Vector3(0, 0, 1f);
+            transform.Translate(input * vitesse * Time.deltaTime, Space.World);
+
+
         }
         else
         {
+            if(Input.GetButtonDown("Jump"))
+            {
+                rb.AddForce(Vector3.up * hauteurSaut);
+            }
+            
             hAxis = Input.GetAxis("Horizontal");
-            input = new Vector3(hAxis, 0, 0.5f);
+            input = new Vector3(hAxis, 0, 1f);
             transform.Translate(input * vitesse * Time.deltaTime, Space.World);
         }
        
 	}
     void OnCollisionEnter(Collision collision)
     {
-        /*if(collision.gameObject.tag == "Plancher")
-            IsGrounded = true;*/
+        if(collision.gameObject.tag == "Plancher")
+            IsGrounded = true;
       
     }
 
