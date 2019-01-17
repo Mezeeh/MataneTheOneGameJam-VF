92bddc7546adc6db105db252a6f083556f212d56
Sedrick <sedrickidd@hotmail.com>
2019-01-16 19:58:29 -0500

    ajout de la gestion du score au checkpoints

--- Assets/Scripts/CheckpointControler.cs
+++ Assets/Scripts/CheckpointControler.cs
@@ -11,8 +11,11 @@ public class CheckpointControler : MonoBehaviour {
     public Vector3 checkpoint3;
     public Vector3 checkpoint4;
     private Vector3 lastCheckpoint;
+    private Score score;
+    private PhysiqueLiquide liquide;
     void Start () {
-        
+        liquide = transform.GetComponent<PhysiqueLiquide>();
+        score = transform.GetComponent<Score>();  
 	}
 	
 	void OncollisionEnter(Collision collision)
@@ -20,16 +23,36 @@ public class CheckpointControler : MonoBehaviour {
         switch(collision.gameObject.tag)
         {
             case "Checkpoint1":
-                lastCheckpoint = checkpoint1;
+                if (lastCheckpoint != checkpoint1)
+                {
+                    lastCheckpoint = checkpoint1;
+                    if (liquide.quantiteLiquide < liquide.quantiteMaxLiquide)
+                        remplirBierre();
+                }
                 break;
             case "Checkpoint2":
-                lastCheckpoint = checkpoint2;
+                if (lastCheckpoint != checkpoint2)
+                {
+                    lastCheckpoint = checkpoint2;
+                    if (liquide.quantiteLiquide < liquide.quantiteMaxLiquide)
+                        remplirBierre();
+                }
                 break;
             case "Checkpoint3":
-                lastCheckpoint = checkpoint3;
+                if (lastCheckpoint != checkpoint3)
+                {
+                    lastCheckpoint = checkpoint3;
+                    if (liquide.quantiteLiquide < liquide.quantiteMaxLiquide)
+                        remplirBierre();
+                }
                 break;
             case "Checkpoint4":
-                lastCheckpoint = checkpoint4;
+                if (lastCheckpoint != checkpoint4)
+                {
+                    lastCheckpoint = checkpoint4;
+                    if (liquide.quantiteLiquide < liquide.quantiteMaxLiquide)
+                        remplirBierre();
+                }
                 break;
         }
 
@@ -37,6 +60,14 @@ public class CheckpointControler : MonoBehaviour {
         {
             transform.position = lastCheckpoint;
         }
+    }
+    void remplirBierre()
+    {
+        //lancer l'annimation de refill;
+        var scoreAReduire = Mathf.Floor(liquide.quantiteMaxLiquide - liquide.quantiteLiquide);
+        score.ReduireScore((int)scoreAReduire);
+
+
     }
 	
 }
