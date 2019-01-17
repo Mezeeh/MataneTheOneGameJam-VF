dc8aaecb0e70724ef3398635d45a7e2273f9198a
Unknown <1643216@cgmatane.qc.ca>
2019-01-17 09:47:31 -0500

    Affichage level liquide et controlleur ralenti

---
+++ Assets/Scripts/AffichageLevelLiquide.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AffichageLevelLiquide : MonoBehaviour {
+
+    public Image liquidLevelBar;
+    public GameObject choppe;
+
+    PhysiqueLiquide physiqueLiquide;
+
+    // Use this for initialization
+    void Start () {
+        physiqueLiquide = choppe.GetComponent<PhysiqueLiquide>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		liquidLevelBar.fillAmount = physiqueLiquide.quantiteLiquide / physiqueLiquide.quantiteMaxLiquide;
+	}
+}
