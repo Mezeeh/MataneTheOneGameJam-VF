0e7a1bc170ef96b1f00a4a4dfd59062d3059cb89
Sedrick <sedrickidd@hotmail.com>
2019-01-17 08:16:22 -0500

    Ajout du chargement de classement

--- Assets/Scripts/FenetrePrincipal.cs
+++ Assets/Scripts/FenetrePrincipal.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class FenetrePrincipal : MonoBehaviour {
+    private XmlAccesseur xmlAcc;
+    List<ScoreSaveObject> scores;
+    public Text valeurClassement;
 
+
+    void Start()
+    {
+        xmlAcc = XmlAccesseur.getInstance();
+        scores = new List<ScoreSaveObject>();
+        
+    }
 	public void jouer()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void afficherClassement()
+    {
+        scores = xmlAcc.load();
+        var i = 1;
+        valeurClassement.text = "";
+        foreach(ScoreSaveObject score in scores)
+        {
+            valeurClassement.text += i + " " + score.nom + " " + score.score + "\n";
+            i++;
+        }
+
     }
 
     public void quitter()
