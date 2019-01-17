cbfdd9c0394eaacd210cd42f972e72e16b51ac02
Unknown <1643216@cgmatane.qc.ca>
2019-01-17 08:07:48 -0500

    Prefab avec script vol du ballon

---
+++ Assets/Scripts/FaireVolerBallon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaireVolerBallon : MonoBehaviour {
+
+    float valeurPingPong = 0;
+    float yInitial;
+
+    public float vitesseDeplacement = 2f;
+
+	void Start () {
+        yInitial = transform.position.y;
+	}
+	
+	void Update () {
+        //valeurPingPong = Mathf.PingPong(Time.time * (1 / vitesseDeplacement), 1f);
+        valeurPingPong = Mathf.Sin(Time.time * (1 / vitesseDeplacement));
+        Debug.Log(valeurPingPong);
+        transform.position = new Vector3(transform.position.x, yInitial + (valeurPingPong * 3f), transform.position.z);
+	}
+}
